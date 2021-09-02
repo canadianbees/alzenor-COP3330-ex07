0fda33d2a81117cc213b1750e4776b19995a1fc6
Celina Alzenor <you@example.com>
2021-09-01 21:18:50 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 7 Solution
+ *  Copyright 2021 Celina Alzenor
+ */
+package org.example;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        roomArea livingRoom = new roomArea();
+
+        livingRoom.Calculate();
+    }
+}
---
+++ src/main/java/org/example/roomArea.java
@@ -0,0 +1,40 @@
+
+/*
+ *  UCF COP3330 Fall 2021 roomArea Class file
+ *  Copyright 2021 Celina Alzenor
+ */
+package org.example;
+import java.util.Scanner;
+
+public class roomArea
+{
+    private int length;
+    private int width;
+
+    public roomArea()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is the length of the room in feet? ");
+        this.length = scan.nextInt();
+
+        System.out.print("What is the length of the room in feet? ");
+        this.width =scan.nextInt();
+    }
+
+    public void Calculate()
+    {
+        float C = (float) 0.09290304;
+
+        System.out.println("You entered dimensions of "+this.length+" feet by "+this.width+" feet.");
+
+        int area = this.length * this.width;
+        float areaMeters = area * C;
+
+        System.out.println("The area is \n"+area+" square feet\n"+areaMeters+" square meters.");
+
+
+    }
+
+
+
+}
